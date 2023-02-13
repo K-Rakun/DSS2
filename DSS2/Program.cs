c4926f35703816ced0f143434a6cc262b0133b43
erman şen <erman24@hotmail.com>
2023-02-13 18:22:45 +0300

    first commit

---
+++ DSS2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DSS2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
